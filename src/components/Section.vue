05dd58736b3861c0864c3d0f343f9dd0b4f2d75d
Josefina Espinosa <joc.luz@gmail.com>
2020-10-27 16:01:16 -0300

    Add section component

---
+++ src/components/Section.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="section">
+    <div class="section__header">
+      <div class="section__header__title">{{ title }}</div>
+      <slot name="header"></slot>
+    </div>
+
+    <slot name="body"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Section",
+  props: {
+    title: {
+      type: String,
+      default: ""
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.section__header {
+  display: flex;
+  align-items: center;
+  background: $secondary-color;
+  padding: 5px 10px;
+  color: $main-color;
+  box-shadow: $main-shadow;
+}
+</style>
